d6eda806416774ea2feb8869d50e22cc67102787
Fabrice Luo <fabriceluo@outlook.com>
2021-10-27 14:49:28 +0000

    Add append bashrc.

---
+++ dockerfile/develop/src/modules/bash/bashrc.bash
@@ -0,0 +1,18 @@
+#! /bin/bash
+#
+# bashrc.bash
+# Copyright (C) 2021 fabriceluo <fabriceluo@outlook.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+# bash append config.
+
+# alias
+alias ll='ls -l'
+alias la='ls -A'
+alias l='ls -CF'
+
+
+# default editor
+export EDITOR=vim
