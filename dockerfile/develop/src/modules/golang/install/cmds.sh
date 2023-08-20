395ca188b84151a7bec917bd80f27a10551e03ed
Fabrice Luo <fabriceluo@outlook.com>
2023-08-20 08:47:16 -0700

    [FIX]fix download install bin path errors

---
+++ dockerfile/develop/src/modules/golang/install/cmds.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+# for nvim ale lint
+go install golang.org/x/tools/gopls@latest
+go install mvdan.cc/gofumpt@latest
+go install golang.org/x/tools/cmd/goimports@latest
+go install github.com/segmentio/golines@latest
